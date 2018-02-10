68bd678cd049f0d4ec6c3b3b4b5e44cbd0d3f9ae
Beomjin Park <bumjin@gmail.com>
2018-02-10 12:50:09 +0900

    add login

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const puppeteer = require('puppeteer');
+//dom element selectors
+const USERNAME_SELECTOR = '#loginForm > input.input_id';
+const PASSWORD_SELECTOR = '#loginForm > input.input_pw';
+const BUTTON_SELECTOR = '#loginForm > div.account_submit > button';
+
+const CREDS = require('./creds');
+
 
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  
+
   await page.goto('https://clien.net');
-  await page.screenshot({ path: 'screenshots/clien.png' });
+
+  await page.screenshot({ path: 'screenshots/clien_before_login.png' });
+
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(CREDS.username);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(CREDS.password);
+
+  await page.click(BUTTON_SELECTOR);
+
+  await page.waitForNavigation();
+
+  await page.screenshot({ path: 'screenshots/clien_after_login.png' });
   
   browser.close();
 }
